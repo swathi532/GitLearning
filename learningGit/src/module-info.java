c26caa3971b522e0cd5ffdbea80b9567a634b304
yasho <yasho@LAPTOP-N0245REE>
2021-06-18 16:42:15 -0500

    committing from eclipse

---
+++ learningGit/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author yasho
+ *
+ */
+module learningGit {
+}
---
+++ learningGit/src/sample/VeryFirstClass.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class VeryFirstClass {
+public static void main(String args[]) {
+	//inside the main method
+}
+}
